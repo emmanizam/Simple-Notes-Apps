ebcde1779debf4e08e0712e30e373a405c2415ce
emmanzm <zahramdnzm@gmail.com>
2022-01-15 12:07:16 +0800

    Completed Task 1

--- lib/services/note/note_service_mock.dart
+++ lib/services/note/note_service_mock.dart
@@ -12,31 +12,55 @@ class NoteServiceMock extends NoteService {
   //? Sample of mock data is provided
   final _mockDb = <Note>[
     Note(
+        id: 0,
         title: 'What is Flutter?',
         content:
             'Flutter is an open-source UI framework for creating native mobile applications. It allows developers to build mobile applications a single codebase.'),
     Note(
+        id: 1,
         title: 'Stateful Widgets',
         content:
             'The widgets whose state can be altered once they are built are called stateful Widgets.'),
     Note(
+        id: 2,
         title: 'Conditional (ternary) operator',
         content:
             'The conditional (ternary) operator in Dart takes three operands: a condition, first expression (if truthy) and second expression (if falsy).'),
   ];
 
   @override
-  Future<List<Note>> fetchNotes() async {}
+  Future<List<Note>> fetchNotes() async {
+    return [..._mockDb];
+  }
 
   @override
-  Future<Note> getNote(id) async {}
+  Future<Note> getNote(id) async {
+    final item = _mockDb.firstWhere((note) => note.id == id, orElse: null);
+    return Note.copy(item);
+  }
 
   @override
-  Future<Note> updateNote({id, Note data}) async {}
+  Future<Note> updateNote({id, Note data}) async {
+    final index = _mockDb.indexWhere((note) => note.id == id);
+    if (index == -1){
+      return null;
+    }
+    Note item = Note(id: id, title: data.title, content: data.content);
+    _mockDb[index] = Note.copy(item);
+    return Note.copy(_mockDb[index]);
+  }
 
   @override
-  Future<void> removeNote(id) async {}
+  Future<void> removeNote(id) async {
+    _mockDb.removeWhere((note)=> note.id == id);
+  }
 
   @override
-  Future<Note> addNote(Note data) async {}
+  Future<Note> addNote(Note data) async {
+    int id = (_mockDb != null && _mockDb.length > 0) ? _mockDb.last.id + 1 : 1;
+    data.id = id;
+    Note item = data;
+    _mockDb.add(item);
+    return Note.copy(item);
+  }
 }
--- lib/services/note/note_service_rest.dart
+++ lib/services/note/note_service_rest.dart
@@ -11,20 +11,36 @@ import '../../models/note.dart';
 /// `NoteServiceRest` is a datbase service for notes that are accessed from a REST service.
 ///
 class NoteServiceRest extends NoteService {
-  RestService get rest => locator();
+  RestService get rest => locator<RestService>();
+  final _rest = RestService(baseUrl: 'http://192.168.0.100:3000');
 
   @override
-  Future<List<Note>> fetchNotes() async {}
+  Future<List<Note>> fetchNotes() async {
+    final jsonList = await rest.get('notes');
+    return (jsonList as List).map((json) => Note.fromJson(json)).toList();
+  }
 
   @override
-  Future<Note> getNote(id) async {}
+  Future<Note> getNote(id) async {
+    final json = await rest.get('notes/$id');
+    return Note.fromJson(json);
+  }
 
   @override
-  Future<Note> updateNote({id, Note data}) async {}
+  Future<Note> updateNote({id, Note data}) async {
+    Note item = Note(title: data.title, content: data.content);
+    final json = await rest.patch('notes/$id', data: item);
+    return Note.fromJson(json);
+  }
 
   @override
-  Future<void> removeNote(id) async {}
+  Future<void> removeNote(id) async {
+    await rest.delete('notes/$id');
+  }
 
   @override
-  Future<Note> addNote(Note data) async {}
+  Future<Note> addNote(Note data) async {
+    final json = await rest.post('notes', data: data);
+    return Note.fromJson(json);
+  }
 }
